7b4959d0549480586205139b5a42946c0c92c57e
Matthew Higley <migs@Matthews-MacBook-Pro-2.local>
2019-03-27 20:07:09 -0700

    adding biography

--- src/sections/biography.js
+++ src/sections/biography.js
@@ -1,15 +1,51 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
 class Biography extends Component {
   render() {
     return (
       <section>
         <div className="container">
-          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            In 2008, I completed a certification course at Fresno Institute of
+            Technology for web and graphic design. It wasn't much, but it got my
+            foot in the door for a fantastic journey yet to come. Right out of
+            school I spent eight months as an intern for{" "}
+            <a href="http://bit.ly/1MJ1irL" target="_blank">
+              Hundred10 Design
+            </a>
+            , and it was during this time that I truly began learning the ropes
+            of front-end development. They brought me on full-time as a junior
+            developer and spent two years learning from the best. From there, I
+            joined the marketing team at{" "}
+            <a href="http://bit.ly/1I9hgoP" target="_blank">
+              Paprika Studios
+            </a>{" "}
+            where I developed with PHP and Drupal and added video to my skill
+            set with the help of{" "}
+            <a href="http://bit.ly/1KI5aF4" target="_blank">
+              Meza Films
+            </a>
+            . I later found a place with{" "}
+            <a href="http://bit.ly/1PN9wiw" target="_blank">
+              Edit LLC
+            </a>{" "}
+            and the Buildicus development team. It was there that I was
+            encouraged into exploring teaching and had the honor of developing
+            Geekwise Academy's Bootcamp 2 course, spending 13 rounds sharing my
+            knowledge with hundreds of students coming through the program. I'm
+            now a full-time Developer Fellow for{" "}
+            <a href="http://bit.ly/1JtLxzv" target="_blank">
+              Bitwise Industries
+            </a>{" "}
+            having taught two six-month courses of a specialized class for
+            individuals in partnership with the Fresno DSS and EDC, a semester
+            at the brand new high school Patiño School of Entrepreneurship and
+            recently adding an introduction to Javascript course to my resumé.
+          </p>
         </div>
       </section>
-    )
+    );
   }
 }
 
-export default Biography
+export default Biography;
